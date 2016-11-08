69f77981aa4ff10f187977d654da2733d2c5cd30
lance-ding <lance-ding@outlook.com>
2016-11-08 11:11:54 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// 设置配置
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'lib/views'));
+app.use(express.static(__dirname + '/public'));
+
+// 加载DB配置
+
+// 加载models
+
+// 加载路由
+require('./lib/routes/index.server.routes')(app);
+
+app.listen('3000', function(){
+  console.log('开启服务: 3000 端口');
+});
