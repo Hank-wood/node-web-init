544e031ae233a897f57327851ec855149ce351e4
lance-ding <lance-ding@outlook.com>
2016-11-10 00:55:58 +0800

    配置session

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const session = require('express-session');
+const config = require('./config/config');
 
 // 设置配置
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'lib/views'));
 app.use(express.static(__dirname + '/public'));
+
+// 配置session
+var sess = {
+  secret: config.session_secret,
+  cookie: {}
+};
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1)
+  sess.cookie.secure = true;
+}
+app.use(session(sess));
 
 // 加载DB配置
 require('./config/mongoose');
